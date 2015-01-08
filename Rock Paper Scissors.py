c8862afd8ce19e49bfc9bbe87e59036d3cb3195f
Thomas <tompov227@gmail.com>
2015-01-08 16:56:19 -0500

    Fixed Application Class
    Removed Function Class — joined to Application class

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -11,21 +11,19 @@ def quitWin(w):
 	w.quit()
 
 class Application(object):
-	def win(self):
+	def window(self):
 		self.move = Tk()
 		self.move.title("Pick your move!")
 		self.move.geometry("+100+100")
-		scissors = Button(self.move, text="scissors", font=("Helvetica Neue", "18", "bold"), command= lambda: F.userScissors(self.move))
-		rock = Button(self.move, text="rock", font=("Helvetica Neue", "18", "bold"), command=lambda: F.userRock(self.move))
-		paper = Button(self.move, text="paper", font=("Helvetica Neue", "18", "bold"), command=lambda: F.userPaper(self.move))
+		scissors = Button(self.move, text="scissors", font=("Helvetica Neue", "18", "bold"), command= lambda: self.userScissors(self.move))
+		rock = Button(self.move, text="rock", font=("Helvetica Neue", "18", "bold"), command=lambda: self.userRock(self.move))
+		paper = Button(self.move, text="paper", font=("Helvetica Neue", "18", "bold"), command=lambda: self.userPaper(self.move))
 		quiter = Button(self.move, text="quit", font=("Helvetica Neue", "18", "bold"), command=quit)
 		scissors.pack(pady=7, padx=7, side=LEFT)
 		rock.pack(pady=7, padx=7, side=LEFT)
 		paper.pack(pady=7, padx=7, side=LEFT)
 		quiter.pack(pady=7, padx=7, side=LEFT)
 		self.move.mainloop()
-
-class Functions(object):
 	def __init__(self):
 		self.sc = 0
 		self.pp = 0
@@ -52,11 +50,11 @@ class Functions(object):
 		self.userMove = 'scissors'
 		self.cpuMove = random.choice(choices)
 		if self.cpuMove == 'rock':
-			F.lose(self.userMove, 'rock')
+			self.lose(self.userMove, 'rock')
 		elif self.cpuMove == 'scissors':
-			F.draw(self.userMove)
+			self.draw(self.userMove)
 		elif self.cpuMove == 'paper':
-			F.win(self.userMove, 'paper')
+			self.win(self.userMove, 'paper')
 		self.sc += 1
 		jmove.destroy()
 		jmove.quit()
@@ -64,11 +62,11 @@ class Functions(object):
 		self.userMove = 'paper'
 		self.cpuMove = random.choice(choices)
 		if self.cpuMove == 'rock':
-			F.win(self.userMove, 'rock')
+			self.win(self.userMove, 'rock')
 		elif self.cpuMove == 'scissors':
-			F.lose(self.userMove, 'rock')
+			self.lose(self.userMove, 'rock')
 		elif self.cpuMove == 'paper':
-			F.draw(self.userMove)
+			self.draw(self.userMove)
 		self.pp += 1
 		jmove.destroy()
 		jmove.quit()
@@ -76,11 +74,11 @@ class Functions(object):
 		self.userMove = 'rock'
 		self.cpuMove = random.choice(choices)
 		if self.cpuMove == 'rock':
-			F.draw(self.userMove)
+			self.draw(self.userMove)
 		elif self.cpuMove == 'scissors':
-			F.win(self.userMove, 'scissor')
+			self.win(self.userMove, 'scissor')
 		elif self.cpuMove == 'paper':
-			F.lose(self.userMove, 'paper')
+			self.lose(self.userMove, 'paper')
 		self.rk += 1
 		jmove.destroy()
 		jmove.quit()
@@ -119,7 +117,6 @@ class Functions(object):
 		alert.mainloop()
 
 A = Application()
-F = Functions()
 while 1:
-	A.win()
+	A.window()
 
