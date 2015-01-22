17b05b0da2cedb5cf92cad9df063a2f5fc50250c
Thomas <tompov227@gmail.com>
2015-01-22 17:28:49 -0500

    Finding the GCF using Euclid's Method

---
+++ EuclidsGCF.py
@@ -0,0 +1,15 @@
+def GCF(n1, n2):
+    while n2:
+        if n1 > n2:
+            n1 -= n2
+        elif n2 > n1:
+            n2 -= n1
+        elif n2 == n1:
+            return n1
+
+e = raw_input("Please enter the two numbers separated by a space: ")
+
+while e:
+    x, y = [int(i) for i in e.split()]
+    print(GCF(x, y))
+    e = raw_input("Please enter the two numbers separated by a space: ")
