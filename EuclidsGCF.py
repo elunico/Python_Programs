ee5d64aaa3625f1b89d914fe295b78939542b6bb
Thomas <tompov227@gmail.com>
2015-01-22 17:29:47 -0500

    Added a main

--- EuclidsGCF.py
+++ EuclidsGCF.py
@@ -7,9 +7,12 @@ def GCF(n1, n2):
         elif n2 == n1:
             return n1
 
-e = raw_input("Please enter the two numbers separated by a space: ")
-
-while e:
-    x, y = [int(i) for i in e.split()]
-    print(GCF(x, y))
+def main():
     e = raw_input("Please enter the two numbers separated by a space: ")
+    while e:
+        x, y = [int(i) for i in e.split()]
+        print(GCF(x, y))
+        e = raw_input("Please enter the two numbers separated by a space: ")
+
+if __name__ == '__main__':
+    main()
