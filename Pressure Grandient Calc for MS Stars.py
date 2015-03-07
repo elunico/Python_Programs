777f844c1374c707b4461681a2ab84142133891f
Thomas <tompov227@gmail.com>
2015-03-06 22:33:56 -0500

    Pressure Gradient Calculator using Tkinter

---
+++ Pressure Grandient Calc for MS Stars.py
@@ -0,0 +1,86 @@
+from __future__ import division
+from Tkinter import *
+
+def heq(limit = 45, radius = 23193333, density=4.5, step=45000000):
+    basemass = 10
+    exponent = 1
+    total = 0
+    pressures = []
+    while exponent <= limit:
+        pressure = (((6.6738 * (10 ** -11)) * (basemass ** exponent)) / ((radius ** 2))) * density
+        pressures.append(pressure)
+        total += pressure
+        exponent += 1
+        radius += step
+    return total, pressures
+
+class Application(object):
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Pressure Gradient Calculator")
+        self.root.geometry("+100+30")
+        self.l1 = Label(self.root, text="Enter the limit (45 by default): ")
+        self.l2 = Label(self.root, text="Enter the radius (Solar Radius by default): ")
+        self.l3 = Label(self.root, text="Enter the density (4.5 by default): ")
+        self.l4 = Label(self.root, text="Enter the steps (45,000,000 by default): ")
+        self.e1 = Entry(self.root)
+        self.e2 = Entry(self.root)
+        self.e3 = Entry(self.root)
+        self.e4 = Entry(self.root)
+        self.b1 = Button(self.root, text="Go", command=self.calc)
+        self.l1.grid(row=0, column=0, padx=5, pady=5)
+        self.l2.grid(row=1, column=0, padx=5, pady=5)
+        self.l3.grid(row=2, column=0, padx=5, pady=5)
+        self.l4.grid(row=3, column=0, padx=5, pady=5)
+        self.e1.grid(row=0, column=1, padx=5, pady=5)
+        self.e2.grid(row=1, column=1, padx=5, pady=5)
+        self.e3.grid(row=2, column=1, padx=5, pady=5)
+        self.e4.grid(row=3, column=1, padx=5, pady=5)
+        self.b1.grid(row=4, column=1, padx=5, pady=5)
+        self.root.mainloop()
+    def calc(self):
+        answer = Tk()
+        l = self.e1.get()
+        r = self.e2.get()
+        d = self.e3.get()
+        s = self.e4.get()
+        try:
+            l = float(l)
+            r = float(r)
+            d = float(d)
+            s = float(s)
+        except:
+            if l == "":
+                l = 45
+            if r == "":
+                r = 23193333
+            if d == "":
+                d = 4.5
+            if s == "":
+                s = 45000000
+
+        total, pressures = heq(l, r)
+
+        row = 0
+        col = 0
+        for i in pressures:
+            Label(answer, text=i, font=("Courier New", "15", "bold")).grid(row=row, column=col, padx=5, pady=5)
+            row += 1
+            if row == 18:
+                col += 1
+                row = 0
+        Label(answer, text="Total:", font=("Courier New", "15", "bold")).grid(row=row+1, column=col)
+        Label(answer, text=total, font=("Courier New", "15", "bold")).grid(row=row+1, column=col+1)
+        Button(answer, text="Close", command=answer.destroy).grid(row=row+2, column=col+1)
+        answer.mainloop()
+
+
+
+
+def main():
+    h = Application()
+
+
+
+if __name__ == '__main__':
+    main()
