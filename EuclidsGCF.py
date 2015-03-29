2e4d814ee66adcc7bf54916c70585933bdb4f879
Thomas <tompov227@gmail.com>
2015-03-29 12:58:07 -0400

    Fixes

--- EuclidsGCF.py
+++ EuclidsGCF.py
@@ -9,10 +9,20 @@ def GCF(n1, n2):
 
 def main():
     e = raw_input("Please enter the two numbers separated by a space: ")
+    if e == 'quit':
+        raise SystemExit
     while e:
-        x, y = [int(i) for i in e.split()]
-        print(GCF(x, y))
-        e = raw_input("Please enter the two numbers separated by a space: ")
+        try:
+    	   x, y = [int(i) for i in e.split()]
+        except:
+            e = raw_input("You must enter two numbers separated by a space on the same line: ")
+            if e == "quit":
+                raise SystemExit
+    	    continue
+    	print(GCF(x, y))
+    	e = raw_input("Please enter the two numbers separated by a space: ")
+    	if e == 'quit':
+        	raise SystemExit
 
 if __name__ == '__main__':
     main()
