aa173d869b978d91e3695fbc660de207da04ff6c
Thomas <tompov227@gmail.com>
2015-05-21 19:46:45 -0400

    an addition to stdlib

--- findchars.py
+++ findchars.py
@@ -1,7 +1,10 @@
-def find(ch,string1, charlen=1):
-    pos = []
-    for i in range(0, len(string1) - charlen, charlen):
-        if ch == string1[i:i+charlen]:
-            pos.append(i)
-    r = pos if pos else -1
+def find(phrase,haystack, phrase_len=1):
+    '''A function that finds a phrase of characters
+    of arbitrary length within a string. By default it will
+    search for a phrase (needle) 1 character long'''
+    phrase_list = []
+    for i in range(0, len(haystack) - phrase_len, phrase_len):
+        if phrase == haystack[i:i+phrase_len]:
+            phrase_list.append(i)
+    r = phrase_list if phrase_list else -1
     return r
