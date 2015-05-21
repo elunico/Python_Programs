0310eda87ade51911cdcc620b45afa8147bfe45b
Thomas <tompov227@gmail.com>
2015-05-21 19:55:02 -0400

    Fixed a major flaw. I apologize

--- findchars.py
+++ findchars.py
@@ -1,9 +1,11 @@
 def find(phrase,haystack, phrase_len=1):
     '''A function that finds a phrase of characters
     of arbitrary length within a string. By default it will
-    search for a phrase (needle) 1 character long'''
+    search for a phrase (needle) 1 character long. Returns
+    a list of indexes the begin with the first char of the
+    needle. '''
     phrase_list = []
-    for i in range(0, len(haystack) - phrase_len, phrase_len):
+    for i in range(0, len(haystack) - phrase_len, 1):
         if phrase == haystack[i:i+phrase_len]:
             phrase_list.append(i)
     r = phrase_list if phrase_list else -1
