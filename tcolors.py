a37a30c8ea184728cfaf1caea2208c3213418b40
Thomas <tompov227@gmail.com>
2015-05-21 01:19:48 -0400

    Changing Terminal Colors

---
+++ tcolors.py
@@ -0,0 +1,12 @@
+'''Printing these constants will
+change to the indicated color until 
+otherwise specified by a new color'''
+magenta = purple = '\033[95m'
+blue = '\033[94m'
+green = _pass = Pass = ok = '\033[92m'
+yellow = warn = warning = '\033[93m'
+red = error = fail = '\033[91m'
+end = clear_color = clear = '\033[0m'
+bold = '\033[1m'
+underline = uline = '\033[4m'
+
