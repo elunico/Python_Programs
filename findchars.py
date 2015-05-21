32d3173b2574497e558add02cbd4cea6d94c366c
Thomas <tompov227@gmail.com>
2015-05-21 19:55:41 -0400

    Fixed a fatal error (very sorry)

--- findchars.py
+++ findchars.py
@@ -5,7 +5,7 @@ def find(phrase,haystack, phrase_len=1):
     a list of indexes the begin with the first char of the
     needle. '''
     phrase_list = []
-    for i in range(0, len(haystack) - phrase_len, 1):
+    for i in range(0, len(haystack) - phrase_len, 1): # That '1' was the fatal mistake I needed to fix
         if phrase == haystack[i:i+phrase_len]:
             phrase_list.append(i)
     r = phrase_list if phrase_list else -1
