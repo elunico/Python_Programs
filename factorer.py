f0feee141ca6b7f3454c77e3b755559eb5ebc917
Thomas <tompov227@gmail.com>
2015-05-22 00:33:43 -0400

    No longer done in classes

--- factorer.py
+++ factorer.py
@@ -1,71 +1,65 @@
 # Ported from Ruby
 import math
+EXITWORDS = ['quit', 'exit', 'abort', 'done', 'end']
+NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+def invalid (x):
+  if "." in x :
+    return True
+  for i in x:
+      if i not in NUMBERS:
+        return True
+  try:
+    x = int(x)
+  except Exception:
+    return True
+  return False
 
-class NUMCHK(object):
-  NUMBERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-  def invalid (self, x):
-    if "." in x :
-      return True
-    for i in x:
-        if i not in NUMCHK.NUMBERS:
-          return True
-    try:
-      x = int(x)
-    except Exception:
-      return True
-    return False
-
-  def redoer (self, y):
-    while self.invalid(y) :
-      print "Sorry " + y + " is not a valid integer, please enter an integer"
-      print "\nEnter a number: "
-      y = raw_input()
-    return y
-
-
-class FACTORER(object):
-  EXITWORDS = ['quit', 'exit', 'abort', 'done', 'end']
-  def factor(self, n):
-    factors = []
-    newfactors = []
-    if n < 0 :
-      n = -n
-    if n == 1 :
-      return 1
-    elif n == 2 :
-      return 1, 2
-    elif n == 0 :
-      try:
-        raise Exception
-      except Exception:
-        print "Please Enter a non-zero integer"
-    i = 1
-    while i < (int(math.sqrt(n)) + 1) :
-      if n % i == 0 :
-        factors.append(i)
-      i+=1
-    for j in reversed(factors):
-      newfactors.append(n/j)
-    return factors, newfactors
-
-  def main(self):
-    print "Enter an integer or \"quit\" to quit"
-    num = raw_input()
-    if num in FACTORER.EXITWORDS :
+def redoer (y):
+  while invalid(y) :
+    print "Sorry " + y + " is not a valid integer, please enter an integer"
+    print "\nEnter a number: "
+    y = raw_input()
+    if y in EXITWORDS:
       raise SystemExit
-    num = NUMCHK().redoer(num)
-    num=int(num)
-    fs, nfs = self.factor(num)
-    for i in fs:
-        print(i)
-    for i in nfs:
-        print(i)
-    print("")
+  return y
 
+def factor(n):
+  factors = []
+  newfactors = []
+  if n < 0 :
+    n = -n
+  if n == 1 :
+    return 1
+  elif n == 2 :
+    return 1, 2
+  elif n == 0 :
+    try:
+      raise Exception
+    except Exception:
+      print "Please Enter a non-zero integer"
+  i = 1
+  while i < (int(math.sqrt(n)) + 1) :
+    if n % i == 0 :
+      factors.append(i)
+    i+=1
+  for j in reversed(factors):
+    newfactors.append(n/j)
+  return factors, newfactors
 
 def main():
-    while True:
-        FACTORER().main()
+  print "Enter an integer or \"quit\" to quit"
+  num = raw_input()
+  if num in EXITWORDS :
+    raise SystemExit
+  num = redoer(num)
+  num=int(num)
+  fs, nfs = factor(num)
+  for i in fs:
+      print(i)
+  for i in nfs:
+      print(i)
+  print("")
 
 if __name__ == '__main__':
-    main()
+    while 1:
+      main()
