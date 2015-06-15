e5cc3465ef26dc47b4251b52f661c9c5872a5a00
Thomas <tompov227@gmail.com>
2015-06-15 16:09:20 -0400

    Better functions

--- tcolors.py
+++ tcolors.py
@@ -5,7 +5,7 @@ otherwise specified by a new color'''
 __all__ = ['magenta', 'purple', 'blue', 'green', '_pass', 'Pass', 'ok',
            'yellow', 'warn', 'warning', 'red', 'error', 'fail', 'end',
            'clear_color', 'clear', 'bold', 'underline', 'uline', 
-           'strong']
+           'strong', 'colors', 'changeColor', 'clearAll', 'makeBold', 'makeUnderline']
 
 magenta = purple = '\033[95m'
 blue = '\033[94m'
@@ -16,3 +16,23 @@ end = clear_color = clear = '\033[0m'
 bold = strong = '\033[1m'
 underline = uline = '\033[4m'
 
+colors = {'magenta':magenta, 'purple':purple, 'blue':blue, 'green':green, '_pass':_pass, 'Pass':Pass, 'ok':ok,
+           'yellow':yellow, 'warn':warn, 'warning':warning, 'red':red, 'error':error, 'fail':fail}
+
+def changeColor(color):
+    c = color.lower().strip()
+    try:
+        assert c in colors.keys()
+    except AssertionError:
+        print "You must choose from {}".format(colors.keys())
+        return 1
+    print colors[c]
+
+def clearAll():
+    print end
+
+def makeBold():
+    print bold
+
+def makeUnderline():
+    print underline
