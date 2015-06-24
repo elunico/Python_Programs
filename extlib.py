e7edee831f32c73fdecb7e5e36d77ea9a538168c
Thomas <tompov227@gmail.com>
2015-06-24 01:28:31 -0400

    More Libraries

--- extlib.py
+++ extlib.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 import sys
 import string
 import re
 import random
 import tcolors
+
 
 if "2." in sys.version[0:2]:
     VERSION = 2
@@ -13,17 +15,22 @@ if "2." in sys.version[0:2]:
 else:
     VERSION = 3
     xrange = range
+    long = int
+
 
 ESCAPES = ('\n', '\r', '\t', '\a')
-WHITESPACE = ('\t', '\n', '\x0b', '\x0c', '\r', ' ')
+WHITESPACE = tuple(n for n in string.whitespace)
 LOWERCASE = tuple([i for i in string.lowercase]) if VERSION < 3 else tuple([i for i in string.ascii_lowercase])
-UPPERCASE = tuple([j for j in string.uppercase]) if VERSION < 3 else tuple([i for i in string.ascii_uppercase])
-ALPHABET = tuple([k for k in string.letters]) if VERSION < 3 else tuple([i for i in string.ascii_letters])
+UPPERCASE = tuple([j for j in string.uppercase]) if VERSION < 3 else tuple([j for j in string.ascii_uppercase])
+ALPHABET = tuple([k for k in string.letters]) if VERSION < 3 else tuple([k for k in string.ascii_letters])
 DIGITS = tuple([l for l in string.digits])
+PUNCTUATION = tuple([m for m in string.punctuation])
 EXITWORDS = ('quit', 'exit', 'abort', 'done', 'end')
+
 
 def reverse(array):
     return list(reversed(array))
+
 
 def pop_first(array):
     array.reverse()
@@ -31,20 +38,35 @@ def pop_first(array):
     array.reverse()
     return popd
 
-def contains(needle, haystack):
-    '''Checks for presence of each element of needle in haystack
-    unless needle is a string then checks for needle in haystack'''
-    if isinstance(needle, str):
-        return needle in haystack
-    return str_contains(needle, haystack)
 
-def str_contains(needle, haystack):
-    '''Checks for presence of each element of needle in haystack
-    breaking strings into characters if necessary'''
-    for i in needle:
-        if i in haystack:
+def contains(needle, haystack):
+    return needle in haystack
+
+
+def contains_any(list_of_items, iterable_haystack):
+    '''iterates through the list of item and checks
+    to see if ANY of the elements in the list are
+    in the haystack using list_of_items[i] in
+    iterable_haystack'''
+    for i in list_of_items:
+        if i in iterable_haystack:
             return True
     return False
+
+
+def contains_all(list_of_items, iterable_haystack):
+    for i in list_of_items:
+        if i not in iterable_haystack:
+            return False
+    return True
+
+
+def contains_only(list_of_items, iterable_haystack):
+    for i in iterable_haystack:
+        if i not in list_of_items:
+            return False
+    return True
+
 
 def findWithOverlap(phrase, haystack):
     '''A function that finds a phrase of characters
@@ -53,12 +75,12 @@ def findWithOverlap(phrase, haystack):
     needle. '''
     phrase_list = []
     phrase_len = len(phrase)
-    found = 0
     for i in xrange(0, len(haystack) - phrase_len, 1): # That '1' was the fatal mistake I needed to fix
         if phrase == haystack[i:i+phrase_len]:
             phrase_list.append(i)
     r = phrase_list if phrase_list else -1
     return r
+
 
 def find(phrase, haystack):
     '''A function that finds a phrase of characters
@@ -80,12 +102,14 @@ def find(phrase, haystack):
     r = phrase_list if phrase_list else []
     return r
 
+
 def findWithRe(pattern, string, defaultValue=None):
     match = re.search(pattern, string)
     if not match:
         return defaultValue
     if match:
         return match.group()
+
 
 def isInvalidInt (x):
     '''Determine if x is a valid integer'''
@@ -105,6 +129,7 @@ def isInvalidInt (x):
         return True
     return False
 
+
 def rescanForInt (y):
     '''Continuously ask for input while y is not a valid int'''
     while isInvalidInt(y) :
@@ -114,6 +139,7 @@ def rescanForInt (y):
         if y in EXITWORDS:
             raise SystemExit
     return y
+
 
 def findandprint(needle, haystack, echo=True):
     temp = -1
@@ -141,15 +167,38 @@ def findandprint(needle, haystack, echo=True):
     else:
         print("\nNo matches found in {}".format(haystack))
 
+
+def scan_for_bool(item):
+    try:
+        item = int(item)
+    except:
+        pass
+    try:
+        item = float(item)
+    except:
+        pass
+    if isinstance(item, str):
+        item = item.strip().lower()
+        if item in ("True", "true"):
+            return 1
+        if item in ("False", "false"):
+            return 0
+    if isinstance(item, int):
+        return item
+    if isinstance(item, (bool, float, long)):
+        return int(item)
+
+
 def demo():
     a = input("Hello. Please enter any letters: ").lower().strip()
     b = "".join([LOWERCASE[random.randint(0, len(LOWERCASE)-1)] for i in xrange(400)])
     findandprint(a, b)
-    print; print
+    print(); print()
     g = input("\n\nEnter an integer please: ")
     g = int(rescanForInt(g))
     print("\n\nThank you.")
     print("-*-*-*-*-*-*-*-*-*-END-*-*-*-*-*-*-*-*-*-")
+
 
 if __name__ == '__main__':
     demo()
---
+++ stringlib.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+import sys
+import extlib
+
+
+def usage():
+    print ("Usage:\n$ python stringletters.py [-cps ignore_all] "
+           "[-c ignore_caps] [-s ignore_space] [-p ignore_punctuation]")
+    raise SystemExit
+
+
+def showChars(list_of_chars, string):
+    lst = []
+    for i in list_of_chars:
+        if i in string:
+            lst.append(string.index(i))
+    return lst
+
+
+def fixNonAscii(string):
+    for i in string:
+        if ord(i) > 127:
+            string = string.replace(i, "")
+    return string
+
+
+def isAscii(string):
+    """Checks to make sure all characters in <string>
+    are ASCII returning true if so else false"""
+    return all(ord(c) < 128 for c in string)
+
+
+def findnonAscii(string):
+    """Returns the index of the first non-ASCII
+    character in <string> or -1 if string is
+    ASCII"""
+    nonasciis = []
+    for c in string:
+        if ord(c) >= 128:
+            nonasciis.append(string.index(c) + 1)
+    if not nonasciis:
+        return -1
+    else:
+        return nonasciis
+
+
+def parseArgs(arg_list):
+    ic = ip = is_ = 0
+    if (((not ('-c' in arg_list or '-s' in arg_list or
+         '-p' in arg_list)) and (not ('-cps' in arg_list or
+         '-pcs' in arg_list or '-psc' in arg_list or '-csp' in arg_list or
+         '-scp' in arg_list or '-spc' in arg_list))) and len(arg_list) > 2):
+        # THEN
+        usage()
+    try:
+        if '-c' in arg_list:
+            ic = extlib.scan_for_bool(arg_list[arg_list.index('-c') + 1])
+        if '-p' in arg_list:
+            ip = extlib.scan_for_bool(arg_list[arg_list.index('-p') + 1])
+        if '-s' in arg_list:
+            is_ = extlib.scan_for_bool(arg_list[arg_list.index('-s') + 1])
+        # CURRENTLY NOT ALLOWED DUE TO USAGE CONSTRAINTS
+        if '-cp' in arg_list:
+            ic = ip = extlib.scan_for_bool(arg_list[arg_list.index('-cp') + 1])
+        if '-cs' in arg_list:
+            ic = is_ = extlib.scan_for_bool(arg_list[arg_list.index('-cs') + 1])
+        if '-ps' in arg_list:
+            is_ = ip = extlib.scan_for_bool(arg_list[arg_list.index('-ps') + 1])
+        if '-pc' in arg_list:
+            ic = ip = extlib.scan_for_bool(arg_list[arg_list.index('-pc') + 1])
+        if '-sc' in arg_list:
+            ic = is_ = extlib.scan_for_bool(arg_list[arg_list.index('-sc') + 1])
+        if '-sp' in arg_list:
+            is_ = ip = extlib.scan_for_bool(arg_list[arg_list.index('-sp') + 1])
+        # END
+        if '-cps' in arg_list:
+            is_ = ip = ic = extlib.scan_for_bool(arg_list[arg_list.index('-cps') + 1])
+        if '-csp' in arg_list:
+            is_ = ip = ic = extlib.scan_for_bool(arg_list[arg_list.index('-csp') + 1])
+        if '-spc' in arg_list:
+            is_ = ip = ic = extlib.scan_for_bool(arg_list[arg_list.index('-spc') + 1])
+        if '-scp' in arg_list:
+            is_ = ip = ic = extlib.scan_for_bool(arg_list[arg_list.index('-scp') + 1])
+        if '-psc' in arg_list:
+            is_ = ip = ic = extlib.scan_for_bool(arg_list[arg_list.index('-psc') + 1])
+        if '-pcs' in arg_list:
+            is_ = ip = ic = extlib.scan_for_bool(arg_list[arg_list.index('-pcs') + 1])
+        return (ic, ip, is_)
+    except (IndexError, ValueError, TypeError):
+        usage()
+
+
+def isPangram(string):
+    if not isAscii(string):
+        print("Could not compare; remove the invalid chars: ")
+        print(findnonAscii(string))
+        return False
+    for i in extlib.LOWERCASE:
+        if i not in string:
+            return False
+    return True
+
+
+def isPalindrome(string, ignore_caps=False, ignore_punc=False,
+                 ignore_space=False):
+    if not isAscii(string):
+        print("Could not compare remove the invalid chars: ")
+        print(findnonAscii(string))
+    if ignore_caps:
+        string = string.lower()
+    if ignore_punc:
+        for i in extlib.PUNCTUATION:
+            string = string.replace(i, "")
+    if ignore_space:
+        for i in extlib.WHITESPACE:
+            string = string.replace(i, "")
+    if string[::-1] == string:
+        return True
+    return False
+
+
+def main(argc, argv):
+    ic, ip, is_ = parseArgs(argv)
+    try:
+        argv[1]
+    except IndexError:
+        usage()
+    print(isPalindrome(argv[1], ic, ip, is_))
+
+
+if __name__ == '__main__':
+    main(len(sys.argv), sys.argv)
